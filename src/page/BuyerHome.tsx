3699c4f14772bef8007fcc4637d59b0b1b3fc1a3
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-02-17 15:03:05 +0900

    #2 - Dessign: BuyerHome 생성 및 레이아웃

---
+++ src/page/BuyerHome.tsx
@@ -0,0 +1,32 @@
+import CommonBtn from "../component/CommonBtn";
+import styled from "styled-components";
+import img from "../asset/겨울.gif";
+
+const Layout = styled.main`
+  min-width: 900px;
+  height: 100%;
+  background: url(${img}) no-repeat center/cover;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  gap: 5px;
+`;
+
+function BuyerHome() {
+  return (
+    <Layout>
+      <CommonBtn
+      text="매장에서 먹기" 
+      width="100%"
+      padding="60px"
+       />
+      <CommonBtn
+      text="포장해서 가기" 
+      width="100%"
+      padding="60px"
+       />
+    </Layout>
+  );
+}
+
+export default BuyerHome;
