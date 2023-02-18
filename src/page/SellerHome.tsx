ed4d18ae081685f0c411b49bbdde8b86daee50ca
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-02-18 14:45:00 +0900

    #5 - Design: SellerHome component 생성 및 레이아웃

--- src/page/SellerHome.tsx
+++ src/page/SellerHome.tsx
@@ -5,23 +5,24 @@ import { CommonBtn } from "../component/CommonBtn";
 
 const Layout = styled.main`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100%;
 `;
 
 const Section = styled.section`
-
-h2{
-    font-size:3rem;
-}
+  h2 {
+    font-size: 3rem;
+    padding-bottom: 50px;
+  }
   display: flex;
+  width: 450px;
+  height: 500px;
+  border: 1px solid black;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  border: 1px solid black;
-  width: 450px;
-  height: 500px;
   border-radius: 10px;
   padding: 10px;
   gap: 10px;
@@ -37,17 +38,19 @@ function SellerHome() {
         <CommonInput
           text="아이디"
           state={userId}
-          setState={setUserId}
+          onChange={ e => setUserId(e.target.value)}
           holder={"아이디를 입력해주세요"}
           label={false}
         />
         <CommonInput
           text="비밀번호 확인"
           state={userPassword}
-          setState={setUserPassword}
+          onChange={ e => setUserPassword(e.target.value)}
           holder={"비밀번호를 입력해주세요"}
           label={false}
         />
+          <CommonBtn text="로그인" />
+          <CommonBtn text="회원가입"/>
       </Section>
     </Layout>
   );
