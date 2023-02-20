8678d5913e721745e6ac3e40d1cae6959bcbb8cc
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-02-20 11:20:30 +0900

    #5 - Design: ProductCD component 생성 및 레이아웃

---
+++ src/page/ProductCD.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Layout = styled.div`
+
+`
+
+function ProductCD(){
+    return(
+        <Layout>추가/삭제</Layout>
+    );
+}
+
+export default ProductCD;
