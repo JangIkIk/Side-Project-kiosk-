f01fafa6a1470e9cce64e882a287fa33a2c3e415
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-02-20 16:43:41 +0900

    #11 - Design: 구매자 홈화면 변경

--- src/page/BuyerHome.tsx
+++ src/page/BuyerHome.tsx
@@ -1,30 +1,38 @@
-import { CommonBtn } from "../component/CommonBtn";
 import styled from "styled-components";
-import img from "../asset/겨울.gif";
+import img from "../asset/home1.png";
+import { CommonMoveBtn } from "../component/CommonMoveBtn";
 
 const Layout = styled.main`
+  position: relative;
   min-width: 900px;
-  height: 100%;
-  background: url(${img}) no-repeat center/cover;
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: flex-end;
+  height: 100%;
   gap: 5px;
+  .click-btns{
+    bottom:5px;
+    left:0;
+    right:0;
+    display:flex;
+    position: absolute;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+  }
 `;
 
 function BuyerHome() {
   return (
     <Layout>
-      <CommonBtn
-      text="매장에서 먹기" 
-      width="100%"
-      padding="60px"
-       />
-      <CommonBtn
-      text="포장해서 가기" 
-      width="100%"
-      padding="60px"
-       />
+      <img src={img} />
+      <div className="click-btns">
+        <CommonMoveBtn text="먹고가기" height="100px" href="/select" />
+        <CommonMoveBtn text="포장하기" height="100px" href="/select" />
+      </div>
     </Layout>
   );
 }
