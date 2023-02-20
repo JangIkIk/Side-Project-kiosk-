5e7f9836a654afcebaa1fcb1e34345aa9a7a4bda
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-02-20 11:18:39 +0900

    #5 - Design: ProductComplete component 생성 및 레이아웃

---
+++ src/page/ProductComplete.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Layout = styled.div`
+
+`
+
+function ProductComplete(){
+    return(
+        <Layout>
+            <p>상품완료</p>
+        </Layout>
+    )
+}
+
+export default ProductComplete
