b8d7a28dfbe6def2f1cdaf4432568d9d07792a04
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-02-20 11:19:56 +0900

    #5 - Design: ProductSort component 생성 및 레이아웃

---
+++ src/page/ProductSort.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Layout = styled.div`
+
+`
+
+function ProductSort(){
+    return(
+        <Layout>정렬</Layout>
+    );
+}
+
+export default ProductSort;
