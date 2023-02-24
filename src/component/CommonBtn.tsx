ce7a3e4489a2398952a1a5cbbdc65031c4ee93d6
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-02-24 14:37:53 +0900

    #9 - Refactor: CommonBtn 기능추가

--- src/component/CommonBtn.tsx
+++ src/component/CommonBtn.tsx
@@ -1,22 +1,30 @@
 import styled from "styled-components";
-
+import * as React from "react";
 
 interface Props {
     text?: string;
     width?: string;
     radius?: string;
     padding?: string;
+    font?: number;
+    background?: string;
 }
   
 
 const Layout = styled.button<Props>`
-  color: white;
-  font-size: 2rem;
-  background-color: #0500ff;
+  color: black;
+  font-size: ${ props => props.font ?? 2}rem;
+  background-color: ${ props => props.background ?? "#0500ff"};
   width: ${ props => props.width ?? "100%"};
   padding: ${ props => props.padding ?? "15px"};
   border-radius: ${ props => props.radius ?? "10px"};
   cursor: pointer;
+  font-weight: bolder;
+
+  &:hover{
+    background-color:gray;
+    color:white;
+  }
 `;
 
 export const CommonBtn = ({
@@ -24,11 +32,16 @@ export const CommonBtn = ({
   width,
   radius,
   padding,
+  font,
+  background,
 }: Props): JSX.Element => {
+
   const styles = {
     width,
     radius,
     padding,
+    font,
+    background,
   };
   return <Layout {...styles}>{text ?? "사용자입력"}</Layout>;
 };
