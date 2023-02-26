f6065eff94d4b338a80d42ab536b7d5ade91a9aa
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-02-26 17:54:50 +0900

    #10 - Refactor: CarteGory component 타입변경

--- src/component/CarteGory.tsx
+++ src/component/CarteGory.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { CommonBtn } from "./CommonBtn";
 
 interface Props {
-  data: any;
+  data: Object[];
 }
 
 const Layout = styled.div`
@@ -29,7 +29,7 @@ export const CarteGory = ({ data }: Props) => {
   return (
     <Layout>
       <div className="cartegory-box">
-        {data.map((el : any) => {
+        {data.map((el : {id: number , title:string}) => {
           return (
             <div className="box-items" key={el.id}>
               <CommonBtn 
